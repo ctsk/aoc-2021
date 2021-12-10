72ea7bf78ad828b81600228d7142fd5a3c1f9920
ctsk <9384305+ctsk@users.noreply.github.com>
2021-12-10 11:20:47 +0100

    Day 10, prettier

--- python/day10.py
+++ python/day10.py
@@ -1,58 +1,63 @@
-inp = open("../data/10.in").read().strip().splitlines()
+def parse(file):
+    return file.read().strip().splitlines()
 
-err = 0
-inc = []
-broken = []
-for line in inp:
-    l = []
-    err0 = err
-    for i in line:
-        if i in ['(', '{', '[', '<']:
-            l.append(i)
+opn = frozenset(('(', '{', '[', '<'))
+clos = frozenset((')', '}', ']', '>'))
 
-        elif i == ')' and l[-1] != '(':
-            err += 3
-            break
-        elif i == ']' and l[-1] != '[':
-            err += 57
-            break
-        elif i == '}' and l[-1] != '{':
-            err += 1197
-            break
-        elif i == '>' and l[-1] != '<':
-            err += 25137
-            break
-        else:
-            l.pop()
+penalty = {
+   ')': 3,
+   '}': 57,
+   ']': 1197,
+   '>': 25137
+}
 
-    if err == err0:
-        inc.append(line)
-print(err)
-errs = []
-for line in inc:
-    l = []
-    for i in line:
-        if i in ['(', '{', '[', '<']:
-            l.append(i)
-        else:
-            l.pop()
-    err = 0
-    for i in l[::-1]:
-        if i == '(':
-            err *= 5
-            err += 1
-        if i == '[':
-            err *= 5
-            err += 2
-        if i == '{':
-            err *= 5
-            err += 3
-        if i == '<':
-            err *= 5
-            err += 4
-    l.pop()
-    errs.append(err)
+score = {
+   '(': 1,
+   '{': 2,
+   '[': 3,
+   '<': 4
+}
 
-errs.sort()
-print(errs[len(errs) // 2])
+complementary = {
+   ')': '(',
+   '}': '{',
+   ']': '[',
+   '>': '<'
+}
 
+def calculate_correction(opn_stack):
+    correction = 0
+    for char in opn_stack[::-1]:
+        correction *= 5
+        correction += score[char]
+    return correction
+
+def part_1_and_2(inp):
+    error_score = 0
+    correction_scores = []
+    for line in inp:
+        stack = []
+        error_before_line = error_score
+        for char in line:
+            if char in opn:
+                stack.append(char)
+            elif char in clos:
+                if stack[-1] == complementary[char]:
+                    stack.pop()
+                else:
+                    error_score += penalty[char]
+                    break
+        if (error_score == error_before_line):
+            correction_scores.append(calculate_correction(stack))
+    correction_scores.sort()
+    return error_score, correction_scores[len(correction_scores) // 2]
+
+def main():
+    with open("../data/10.in", "r") as f:
+        inp = parse(f)
+        p1, p2 = part_1_and_2(inp)
+        print(p1)
+        print(p2)
+
+if __name__ == "__main__":
+    main()
