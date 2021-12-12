40c01a719423e7139cafda775e18b43b019a3b98
ctsk <9384305+ctsk@users.noreply.github.com>
2021-12-12 11:11:46 +0100

    Day 12 \o/

---
+++ python/day12.py
@@ -0,0 +1,43 @@
+from collections import defaultdict
+
+START = 'start'
+END = 'end'
+
+def parse(file):
+    p = [tuple(line.split("-")) for line in file.read().strip().splitlines()]
+    graph = defaultdict(list)
+    for a, b in p:
+        graph[a].append(b)
+        graph[b].append(a)
+    return graph
+
+def search(g, u, path, allowDouble):
+    count = 0
+    for v in g[u]:
+        if v == END:
+            count += 1
+        elif v.isupper():
+            count += search(g, v, path, allowDouble)
+        elif v not in path:
+            path.add(v)
+            count += search(g, v, path, allowDouble)
+            path.remove(v)
+        elif allowDouble and v != START:
+            count += search(g, v, path, False)
+    return count
+
+def part1(graph):
+    return search(graph, START, {START}, False)
+
+def part2(graph):
+    return search(graph, START, {START}, True)
+
+def main():
+    with open("../data/12.in", "r") as f:
+        inp = parse(f)
+        print(part1(inp))
+        print(part2(inp))
+
+if __name__ == "__main__":
+    main()
+
